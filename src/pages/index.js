a571e2dd2a51186fd528899b79131574253caca3
EstherUrzanqui <estherurzanqui81@gmail.com>
2021-03-27 18:54:17 +0100

    update CV

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,7 +49,7 @@ const IndexPage = () => (
             I’m confident, naturally problem solver and always working to improve myself professional and personally. 
             </p>
             {/*<Scroll type="id" element="myCv" offset={100}>*/}
-              <a className="btn btn-dark btn-xl" href="https://drive.google.com/file/d/1xO8b_4xYZsjWuy9ws-yrA3q87vTAw1yv/view?usp=sharing" target="_blank" rel = "noopener noreferrer">
+              <a className="btn btn-dark btn-xl" href="https://drive.google.com/file/d/1Xtt0BkdQAdx0uHZ-T_OxmUR_tjvAaT-K/view?usp=sharing" target="_blank" rel = "noopener noreferrer">
                 My CV
               </a>
             {/*</Scroll>*/}
